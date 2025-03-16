d3048228adfd1d1d0fc75afe53487551afefffbd
Sude Ölmez <sudeolmez@MacBookAir.hgw.local>
2025-03-16 18:20:11 +0300

    langsmith integrated

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,10 @@ from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.store.memory import InMemoryStore
 import uuid
+from dotenv import load_dotenv
+from langsmith.run_trees import RunTree
 
+load_dotenv() 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
@@ -16,6 +19,13 @@ class ChatBot:
         self.checkpointer = InMemorySaver()
         self.store = InMemoryStore()
         
+        self.pipeline = RunTree(
+            name="Chat Pipeline",
+            run_type="chain",
+            inputs={"question": ""}  
+        )
+        self.pipeline.post()
+
         self.graph_builder = StateGraph(State)
         self._build_graph()
 
@@ -38,6 +48,12 @@ class ChatBot:
         else:
             new_message = {"role": "user", "content": f"{user_input}"}
         
+        self.pipeline.create_child(
+            name="Analyze Message",
+            run_type="tool",
+            inputs={"user_input": user_input}
+        ).post()
+
         state["messages"][-1] = new_message
         return state 
 
@@ -45,17 +61,34 @@ class ChatBot:
         response = self.llm.invoke(state["messages"])
         chatbot_message = {"role": "assistant", "content": str(response)} 
         state["messages"][-1] = chatbot_message
+
+        self.pipeline.create_child(
+            name="Chatbot Call",
+            run_type="llm", 
+            inputs={"messages": state["messages"]}
+        ).post()
+
         return state
 
     def generate_final_response(self, state: State):
         search_result = state["messages"][-1].content
         final_response = {"role": "assistant", "content": f"Based on the query '{search_result}', the result is: {search_result}"}
         state["messages"][-1] = final_response
+
+        self.pipeline.create_child(
+            name="Generate Final Response",
+            run_type="tool",
+            inputs={"search_result": search_result}
+        ).post()
+
         return state  
 
     def stream_graph_updates(self, user_input: str):
         state = {"messages": [{"role": "user", "content": user_input}]}
         config = {"configurable": {"thread_id": "1"}}
+
+        self.pipeline.inputs["question"] = user_input
+        self.pipeline.patch()
         
         for event in self.graph.stream(state, config):
             for value in event.values():
@@ -63,3 +96,6 @@ class ChatBot:
                     print("Assistant:", value["messages"][-1]["content"])
                 else:
                     print("Error: Received None, skipping...")
+        
+        self.pipeline.end(outputs={"answer": state["messages"][-1]["content"]})
+        self.pipeline.patch()
